a5a15198690a1a8ab8154b50be58283cc2b7d50c
adibmahmud007 <adibmahmud307@gmail.com>
2024-01-11 20:27:27 +0600

    Tailwind and Daisy setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl text-center '>Hello world</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
